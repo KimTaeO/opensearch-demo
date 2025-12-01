3b6a3b8aec17656f17c0fa7d8cfe60770aa50c98
KimTaeO <taeo0328@gmail.com>
2025-12-01 18:55:06 +0900

    add :: configuration properties scan annotation

--- src/main/kotlin/com/example/opensearch/DemoApplication.kt
+++ src/main/kotlin/com/example/opensearch/DemoApplication.kt
@@ -1,9 +1,12 @@
 package com.example.opensearch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class DemoApplication
 
 fun main(args: Array<String>) {

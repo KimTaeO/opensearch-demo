c899350c54006d302a9d97163836146fa20ff032
KimTaeO <taeo0328@gmail.com>
2025-12-07 13:43:09 +0900

    update :: use configuration properties & set basic auth

--- src/main/kotlin/com/example/opensearch/config/OpenSearchConfig.kt
+++ src/main/kotlin/com/example/opensearch/config/OpenSearchConfig.kt
@@ -1,19 +1,37 @@
 package com.example.opensearch.config
 
+import com.example.opensearch.config.properties.OpenSearchProperties
+import org.apache.hc.client5.http.auth.AuthScope
+import org.apache.hc.client5.http.auth.UsernamePasswordCredentials
+import org.apache.hc.client5.http.impl.async.HttpAsyncClientBuilder
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider
 import org.apache.hc.core5.http.HttpHost
+import org.apache.hc.core5.http2.HttpVersionPolicy
 import org.opensearch.client.json.jackson.JacksonJsonpMapper
 import org.opensearch.client.opensearch.OpenSearchClient
 import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBuilder
 import org.springframework.context.annotation.Configuration
 
-@Configuration
-class OpenSearchConfig {
-    fun openSearchClient(): OpenSearchClient {
-        val httpHost = HttpHost("http", "localhost", 9200)
 
+@Configuration
+class OpenSearchConfig(
+    private val openSearchProperties: OpenSearchProperties
+) {
+    fun openSearchClient(): OpenSearchClient {
+        val httpHost = HttpHost(openSearchProperties.protocol, openSearchProperties.host, openSearchProperties.port)
+
+        val credentialsProvider = BasicCredentialsProvider()
+        credentialsProvider.setCredentials(
+            AuthScope(httpHost),
+            UsernamePasswordCredentials("admin", "Asdf12345678!".toCharArray())
+        )
         val opensearchTransport =
             ApacheHttpClient5TransportBuilder
                 .builder(httpHost)
+                .setHttpClientConfigCallback {
+                    it.setDefaultCredentialsProvider(credentialsProvider)
+                    it.setVersionPolicy(HttpVersionPolicy.FORCE_HTTP_2)
+                }
                 .setMapper(JacksonJsonpMapper())
                 .build()
 
